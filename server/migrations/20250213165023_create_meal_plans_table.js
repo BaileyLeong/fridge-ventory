489f969821961915881728c43f7384f281689249
BaileyLeong <baileyleong@gmail.com>
2025-02-13 13:23:20 -0500

    recreate "create_meal_plans" migration file

--- server/migrations/20250213165023_create_meal_plans_table.js
+++ server/migrations/20250213165023_create_meal_plans_table.js
@@ -1,15 +1,23 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+export const up = function (knex) {
+  return knex.schema.createTable("meal_plans", function (table) {
+    table.increments("id").primary();
+    table.integer("user_id").unsigned().notNullable();
+    table.integer("recipe_id").unsigned().notNullable();
+    table.date("planned_date").notNullable();
+
+    table
+      .foreign("user_id")
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE");
+    table
+      .foreign("recipe_id")
+      .references("id")
+      .inTable("recipes")
+      .onDelete("CASCADE");
+  });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+export const down = function (knex) {
+  return knex.schema.dropTable("meal_plans");
 };
