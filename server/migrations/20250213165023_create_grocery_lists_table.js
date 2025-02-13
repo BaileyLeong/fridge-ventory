f488c27af91abbaad55c59715a37e79a794af258
BaileyLeong <baileyleong@gmail.com>
2025-02-13 13:14:37 -0500

    recreate "grocery_list" migration files

--- server/migrations/20250213165023_create_grocery_lists_table.js
+++ server/migrations/20250213165023_create_grocery_lists_table.js
@@ -1,15 +1,25 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+export const up = function (knex) {
+  return knex.schema.createTable("grocery_lists", function (table) {
+    table.increments("id").primary();
+    table.integer("user_id").unsigned().notNullable();
+    table.integer("ingredient_id").unsigned().notNullable();
+    table.decimal("quantity", 10, 2).notNullable().defaultTo(0);
+    table.string("unit", 50).notNullable().defaultTo("unit");
+    table.boolean("completed").defaultTo(false);
+
+    table
+      .foreign("user_id")
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE");
+    table
+      .foreign("ingredient_id")
+      .references("id")
+      .inTable("ingredients")
+      .onDelete("CASCADE");
+  });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+export const down = function (knex) {
+  return knex.schema.dropTable("grocery_lists");
 };
