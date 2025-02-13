2cd495e1792016086f348eb94cb396ef852b57e1
BaileyLeong <baileyleong@gmail.com>
2025-02-13 12:08:14 -0500

    recreate "users" table migration file

--- server/migrations/20250213165022_create_users_table.js
+++ server/migrations/20250213165022_create_users_table.js
@@ -1,15 +1,11 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+export const up = function (knex) {
+  return knex.schema.createTable("users", function (table) {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.string("email").unique().notNullable();
+  });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+export const down = function (knex) {
+  return knex.schema.dropTable("users");
 };
