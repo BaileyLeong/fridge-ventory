53a9fc6ab308c14324c071eb84c6185a42ff1c05
BaileyLeong <baileyleong@gmail.com>
2025-02-13 12:28:20 -0500

    recreate "recipes" migration file

--- server/migrations/20250213165022_create_recipes_table.js
+++ server/migrations/20250213165022_create_recipes_table.js
@@ -1,15 +1,16 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+export const up = function (knex) {
+  return knex.schema.createTable("recipes", function (table) {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.string("category");
+    table.string("image_url").defaultTo("https://placehold.co/100");
+    table.integer("ready_in_minutes");
+    table.integer("servings");
+    table.string("source_url");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+  });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+export const down = function (knex) {
+  return knex.schema.dropTable("recipes");
 };
