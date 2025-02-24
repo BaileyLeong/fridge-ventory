d83dab495649fe1771e3ffe14afe253d8ba38786
BaileyLeong <baileyleong@gmail.com>
2025-02-24 00:40:08 -0500

    update sizing

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -13,51 +13,48 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <button className="navbar__toggle" onClick={toggleMenu}>
-        <img src={menuIcon} alt="menu icon" />
-      </button>
-      <h1 className="navbar__title">Fridge-Ventory</h1>
+      <div className="navbar__container">
+        <button className="navbar__toggle" onClick={toggleMenu}>
+          <img src={menuIcon} alt="menu icon" />
+        </button>
+        <h1 className="navbar__title">Fridge-Ventory</h1>
 
-      <ul className={`navbar__list ${menuOpen ? "navbar__list--open" : ""}`}>
-        <li className="navbar__item">
-          <NavLink to="/dashboard" className="navbar__link">
-            Dashboard
-          </NavLink>
-        </li>
-        <li className="navbar__item">
-          <NavLink to="/fridge" className="navbar__link">
-            Fridge
-          </NavLink>
-        </li>
-        <li className="navbar__item">
-          <NavLink to="/recipes" className="navbar__link">
-            Recipes
-          </NavLink>
-        </li>
-        <li className="navbar__item">
-          <NavLink to="/meal-planner" className="navbar__link">
-            Meal Planner
-          </NavLink>
-        </li>
-        <li className="navbar__item">
-          <NavLink to="/grocery-list" className="navbar__link">
-            Grocery List
-          </NavLink>
-        </li>
-        <li className="navbar__item">
-          <NavLink to="/favorites" className="navbar__link">
-            Favorites
-          </NavLink>
-        </li>
-        <li className="navbar__item">
-          <NavLink
-            to="/surprise-me"
-            className="navbar__link navbar__link--special"
-          >
-            I'm Bored!
-          </NavLink>
-        </li>
-      </ul>
+        <ul className={`navbar__list ${menuOpen ? "navbar__list--open" : ""}`}>
+          <li className="navbar__item">
+            <NavLink to="/dashboard" className="navbar__link">
+              Dashboard
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink to="/fridge" className="navbar__link">
+              Fridge
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink to="/meal-planner" className="navbar__link">
+              Meal Planner
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink to="/grocery-list" className="navbar__link">
+              Grocery List
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink to="/favorites" className="navbar__link">
+              Favorites
+            </NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink
+              to="/surprise-me"
+              className="navbar__link navbar__link--special"
+            >
+              I'm Bored!
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
